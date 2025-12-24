ff8c7621cf7a12394aec5be0229becf04b833c16
Vivek Jha <progamervivek2020@gmail.com>
2025-12-24 21:11:05 +0530

    Add Hero component with image and update styling for improved layout

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import img from "../assets/Real.jpg";
+import Header from "./header.jsx";
 
 function Hero({ top }) {
   return (
     <div
-      className={`w-screen h-screen flex border-2 border-dashed p-10 items-center m-${top} justify-center `}
+      className={`w-screen h-screen flex border-2 border-white rounded-3xl items-center m-${top} justify-center `}
     >
-      <div className="border-2 border-dashed w-3/4 h-3/4"></div>
+      <div className="border-2 border-white rounded-3xl mt-16 w-23/24 h-21/24">
+        <img
+          src={img}
+          alt="Hero"
+          className="w-full h-full object-cover rounded-3xl"
+        />
+      </div>
     </div>
   );
 }
