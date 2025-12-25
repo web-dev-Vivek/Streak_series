f6cc26a1ee356046215d9999681b351c89f1dca5
Vivek Jha <progamervivek2020@gmail.com>
2025-12-25 18:44:41 +0530

    Remove unused components and clear out App structure; delete Real.jpg asset

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import HEADER from "./component/header";
-import Footer from "./component/Footer";
-import IsloggedIn from "./component/isloggedin";
-import Usercard from "./component/Usercard";
-import Listrender from "./component/Listrender";
-import Wrapper from "./component/wrapper";
-import Hero from "./component/hero";
 import "./App.css";
 
 function increaseCount() {
   console.log("button clicked");
 }
 function App() {
-  return (
-    <>
-      <HEADER />
-
-      <Hero />
-      {/* <Footer /> */}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
